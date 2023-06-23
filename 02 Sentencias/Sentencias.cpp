0fa4f439b867874651da88afd0804a8f52fc0fc9
ClaireRupikaING <a.rodriguez.c464@edu.utc.mx>
2023-06-23 09:58:16 -0600

    Ejemplito

---
+++ 02 Sentencias/Sentencias.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+int i=1
+
+;int main(){
+*/do{
+cout<<"Imprimir núeros"<<end1;
+int i=1;
+cout<<i<<end1;
+i--;
+}while(i<11);
+
+
+//while mientras
+
+int count = 1;
+while(count)<=10){
+	cout<<"Imprimir Números"<<end1;
+	
+	cout<<count<<end1;
+	count++;
+	}
+}
+*/
+
+int seleccion;
+
+do{
+	cout<<"\n Menu 1, 2, 3, y otra opción es salir"
+	cout<<"Opcion1\n"
+	cout<<"Opcion2\n"
+	cout<<"Opcion3\n"
+	cout<<"Salir\n"
+	
+	cin>>seleccion;
+	
+	switch(seleccion){
+		case 1:
+			cout<<"Esta es la opción 1";
+			break;
+		Case 2:
+		    cout<<"Esta es la opción 2";
+			break;
+		Case 3:
+		    cout<<"Esta es la opción 3";
+		default:
+		    cout<<"bye bye";
+			break;
+}
+}while((seleccion)>0)
+
